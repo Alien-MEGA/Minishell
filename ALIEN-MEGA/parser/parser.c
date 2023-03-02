be4936c4fe07cc7a0252f15c59bc47fade1f1415
Redouane Iben Hamou <reben-ha@e2r6p13.1337.ma>
2023-03-02 23:51:32 +0100

    Add

--- ALIEN-MEGA/parser/parser.c
+++ ALIEN-MEGA/parser/parser.c
@@ -6,13 +6,25 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 17:22:05 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/03/02 21:40:10 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/03/02 23:51:08 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+/* && → || → | → Word */
 
-// void	parser()
-// {
+void	Def_pri();
+{
 	
-// }
+}
+
+void	check_syntax()
+{
+	
+}
+
+void	parser()
+{
+	check_syntax();
+	Def_pri();
+}
