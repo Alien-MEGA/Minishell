6ffc2ea90aa3c68660298bddeeea165d531edbb4
Redouane Iben Hamou <reben-ha@e2r6p13.1337.ma>
2023-03-06 21:13:29 +0100

    Add

---
+++ ALIEN-MEGA/parser/manage_tree.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   manage_tree.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/06 20:27:22 by reben-ha          #+#    #+#             */
+/*   Updated: 2023/03/06 20:37:24 by reben-ha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+t_tree	*create_command(t_list *lst, int *i)
+{
+	while (in(lst, *i))
+	{
+		
+	}
+	
+}
+
+t_tree	*create_operator(t_list *lst, int *i, int o_type)
+{
+	
+}
+
+t_tree	*create_redirect(t_list *lst, int *i, int o_type)
+{
+	
+}
--- ALIEN-MEGA/parser/parser.c
+++ ALIEN-MEGA/parser/parser.c
@@ -3,22 +3,138 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebennamr <ebennamr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 17:22:05 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/03/03 16:23:17 by ebennamr         ###   ########.fr       */
+/*   Updated: 2023/03/06 20:24:16 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-/* && → || → | → Word */
 
-void	Def_pri(void)
+void	print_tree(t_tree *tree, int depth)
 {
+	if (tree == NULL)
+		return ;
+	print_tree(tree->right, depth + 4);
+	for (int i = 0; i < depth; i++)
+		printf(" ");
+	printf("%s\n", tree->value);
+	print_tree(tree->left, depth + 4);
+}
+
+void	print_lst(t_list *head)
+{
+	printf("[ ");
+	for (size_t i = 0; in(head, i); i++)
+		printf("%s --> ", (char *)(in(head, i)->data));
+	printf("NULL ]\n\n");
+}
+
+int	check_type(t_list *lst, int type)
+{
+	int	i;
+	t_token *tk;
+
+	i = 0;
+	while (in(lst, i))
+	{
+		tk = (t_token *)in(lst, i)->data;
+		if (type == tk->type)
+			return (TRUE);
+		i++;	
+	}
+	return (FALSE);
+}
+
+char	**make_cmd(t_list *lst, int start, int end)
+{
+	char	*cmd;
+
+	cmd = NULL;
+	while (start < end)
+	{
+		cmd = ft_strjoin_gnl(cmd, ((t_token *)in(lst, start)->data)->value);
+		cmd = ft_strjoin_gnl(cmd, " ");	
+		start++;
+	}
+	printf("==============> Print Cmd <============== \n");
+		printf("%s\n", cmd);
+	return (cmd);	
+}
+
+void	parser(t_list *lst)
+{
+	t_tree	*tree;
+	int		i;
+
+	i = -1;
+	while (in(lst, ++i))
+	{
+		if (in(lst, i)->type == TK_RD_OUTPUT
+			|| in(lst, i)->type == TK_RD_OUTPUT_APPEND
+			|| in(lst, i)->type == TK_RD_INPUT
+			|| in(lst, i)->type == TK_HERE_DOC)
+			create_redirect(lst, &i, in(lst, i)->type);
+		else if (in(lst, i)->type == TK_PIPE
+			|| in(lst, i)->type == TK_OR
+			|| in(lst, i)->type == TK_AND)
+			create_operator(lst, &i, in(lst, i)->type);
+		else if (in(lst, i)->type == TK_WORD)
+			create_command(lst, &i);
+	}
+	
+	
+
+
+
+
+
+
+
 
 }
 
-void	parser()
+void	test()
 {
-	Def_pri();
+	t_list	*lst;
+
+	ft_lstadd_back(&lst, ft_lstnew(TK_WORD, ft_strdup("echo"), NULL));
+
+	ft_lstadd_back(&lst, ft_lstnew(TK_WORD, ft_strdup("hoot"), NULL));
+
+	ft_lstadd_back(&lst, ft_lstnew(TK_PIPE, ft_strdup("|"), NULL));
+
+	ft_lstadd_back(&lst, ft_lstnew(TK_WORD, ft_strdup("cat"), NULL));
+
+	ft_lstadd_back(&lst, ft_lstnew(TK_PIPE, ft_strdup("&&"), NULL));
+
+	ft_lstadd_back(&lst, ft_lstnew(TK_WORD, ft_strdup("echo"), NULL));
+
+	ft_lstadd_back(&lst, ft_lstnew(TK_WORD, ft_strdup("gam"), NULL));
+
+	ft_lstadd_back(&lst, ft_lstnew(TK_PIPE, ft_strdup("|"), NULL));
+
+	ft_lstadd_back(&lst, ft_lstnew(TK_WORD, ft_strdup("cat"), NULL));
+
+	ft_indexing(lst);
+	print_lst(lst);
+	parser(lst);
+
+	// print_tree(tree, 10);
 }
+
+/* ====> Exemple : echo hoot | cat && echo gam | cat <====== */
+/* ==============> TEST <============== */
+	// t_tree	*tree;
+
+	// tree = ft_treenew("&&", TK_AND);
+
+	// ft_treeadd_back(&tree, ft_treenew("|", TK_PIPE), RIGHT);
+	// ft_treeadd_back(&tree, ft_treenew("|", TK_PIPE), LEFT);
+
+	// ft_treeadd_back(&tree->left, ft_treenew("cat", TK_WORD), RIGHT);
+	// ft_treeadd_back(&tree->left, ft_treenew("ls", TK_WORD), LEFT);
+
+	// ft_treeadd_back(&tree->right, ft_treenew("cat", TK_WORD), LEFT);
+	// ft_treeadd_back(&tree->right, ft_treenew("ls", TK_WORD), RIGHT);
