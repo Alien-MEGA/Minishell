02b2a41c2445d2c8606a3dc31ffe15a845a31240
Redouane Iben Hamou <reben-ha@e2r6p13.1337.ma>
2023-03-06 18:06:42 +0100

    Add

--- ALIEN-MEGA/string.c
+++ ALIEN-MEGA/string.c
@@ -6,7 +6,7 @@
 /*   By: ebennamr <ebennamr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 16:53:30 by ebennamr          #+#    #+#             */
-/*   Updated: 2023/02/27 18:49:23 by ebennamr         ###   ########.fr       */
+/*   Updated: 2023/03/06 17:52:34 by ebennamr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,45 @@ int	indexofchar(char *line, char c)
 	}
 
 	return (-1);
+}
+
+static void	mat_copy_free(char **res, char **s1,char **s2)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (s1[j])
+	{
+		res[i] = s1[j];
+		i++;
+		j++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[i] = NULL;
+	free(s1);
+	free(s2);
+}
+char	**mat_join(char **s1, char **s2)
+{
+	char	**pt2;
+	char	**pt1;
+	char	**res;
+
+	pt1 = ft_matrixdup(s1);
+	pt2 = ft_matrixdup(s2);
+	res = (char **)malloc((ft_matlen(s1) + ft_matlen(s2) + 1) * sizeof(char *));
+	ft_error_str(res, 1);
+	printf("bfore copy and free \n");
+	mat_copy_free(res, pt1, pt2);
+	printf("after copy and free \n");
+
+	return (res);
 }
--- ALIEN-MEGA/utils/export.c
+++ ALIEN-MEGA/utils/export.c
@@ -1,26 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebennamr <ebennamr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/29 15:14:15 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/03/06 14:37:03 by ebennamr         ###   ########.fr       */
+/*   Created: 2023/03/05 17:31:34 by ebennamr          #+#    #+#             */
+/*   Updated: 2023/03/06 17:57:34 by ebennamr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 
-t_list	*ft_lstnew(int type, char *value, void *data)
-{
-	t_list	*newnode;
 
-	newnode = (t_list *)malloc(sizeof(t_list));
-	ft_error_str(newnode, 1);
-	newnode->type = type;
-	newnode->value = value;
-	newnode->data = data;
-	newnode->next = NULL;
-	return (newnode);
-}
+#include "../minishell.h"
+// void	export()
+// {
+
+// }
+
+// void export_args(char **args)
+// {
+
+// }
+// void export_to_explist(char *arg)
+// {}
+
+// int valid_arg(char args)
+// {
+
+// }
+// void sort_mat(char **arr)
+// {
+// 	int len;
+// 	int j;
+// 	int i;
+
+// 	len = ft
+
+// }
