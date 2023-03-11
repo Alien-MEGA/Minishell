d9ba6fedad642484c868fa30803c892116c590b7
Redouane Iben Hamou <reben-ha@e2r6p13.1337.ma>
2023-03-11 22:40:27 +0100

    Add

--- ALIEN-MEGA/parser/parser.c
+++ ALIEN-MEGA/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 17:22:05 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/03/11 22:10:40 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/03/11 22:34:20 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ t_tree	*or_and(t_list *lst, int *index)
 	t_tree	*tree;
 
 	tree = pipeline(lst, index);
-	while (in(lst, (*index)) &&
-			(in(lst, (*index))->type == TK_OR
+	while (in(lst, (*index))
+			&& (in(lst, (*index))->type == TK_OR
 			|| in(lst, (*index))->type == TK_AND))
 	{
 		ft_treeswap_root(&tree, create_operator(lst, index), LEFT);
