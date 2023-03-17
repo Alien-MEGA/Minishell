7fb7e8ebb2791b4c17d3b714f2ad7c3b19344a5e
Redouane Iben Hamou <reben-ha@e2r5p14.1337.ma>
2023-03-17 21:17:26 +0100

    Add

--- ALIEN-MEGA/execute/execute.c
+++ ALIEN-MEGA/execute/execute.c
@@ -6,50 +6,93 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 15:44:28 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/03/17 13:58:58 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/03/17 18:45:50 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-t_fd	creat_pipe(void)
+void	run_redirect(t_list *redirect)
+{
+	int	fd_in;
+	int	fd_out;
+
+	fd_in = -2;
+	fd_out = -2;
+	if (!redirect)
+		return ;
+	while (redirect)
+	{
+		if (redirect->type == TK_RD_OUTPUT)
+			fd_out = open(redirect->next->value, O_RDWR | O_TRUNC | O_CREAT, 0644);
+		else if (redirect->type == TK_RD_OUTPUT_APPEND)
+			fd_out = open(redirect->next->value, O_RDWR | O_APPEND | O_CREAT, 0644);
+		ft_error(fd_out, 1);
+		if (redirect->type == TK_RD_INPUT)
+			fd_in = open(redirect->next->value, O_RDONLY);
+		else if (redirect->type == TK_HERE_DOC)
+			fd_in = open(here_doc(redirect->next->value), O_RDONLY);
+		ft_error(fd_in, 1);
+		redirect = redirect->next->next;
+	}
+	if (fd_in != -2)
+		ft_error(dup2(fd_in, STDIN_FILENO), 1);
+	if (fd_out != -2)
+		ft_error(dup2(fd_out, STDOUT_FILENO), 1);
+}
+
+t_fd	create_pipe(void)
 {
 	int 	fd[2];
-	t_fd	pipe_fd;
+	t_fd	fd_pipe;
 
 	ft_error(pipe(fd), 1);
-	pipe_fd.fd_rd = fd[];
-	pipe_fd.fd_wr = fd[];
-	
+	fd_pipe.fd_rd = fd[0];
+	fd_pipe.fd_wr = fd[1];
+	return (fd_pipe);	
 }
 
 unsigned int	execute(t_tree *root, int fd_in, int fd_out)
 {
-
+	t_fd			fd_pipe;
+	unsigned int	exit_status;
+	pid_t			pross;
 
 	if (root->lst->type == TK_OR || root->lst->type == TK_AND)
 	{
-		execute(root->left, fd_in, fd_out);
-		execute(root->right, fd_in, fd_out);
+		exit_status = execute(root->left, fd_in, fd_out);
+		exit_status = execute(root->right, fd_in, fd_out);
 	}
 	else if (root->lst->type == TK_PIPE)
 	{
-		creat_pipe();
-		execute(root->left, fd_in, fd_out);
-		execute(root->right, fd_in, fd_out);		
+		fd_pipe = create_pipe();
+		exit_status = execute(root->left, fd_in, fd_pipe.fd_wr);
+		exit_status = execute(root->right, fd_pipe.fd_rd, fd_out);
 	}
-	else if (root->lst->type == TK_WORD)
+	else
 	{
+		run_redirection(root->redirect_mode);
+		pross = fork();
+		ft_error(pross, 1);
+		if (pross == 0)
+		{
+			
+			
+			
+			
+			
+			
+			
 		
-	
-	
-
-
-
-	
-
-
-
+		
+		
+		
+		}
+		
+		
+		
+		
+		
 	}
 
 
