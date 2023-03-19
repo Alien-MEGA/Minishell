ac24425ebf35dec78f6ea9f260fa6aecb5cbc002
Redouane Iben Hamou <reben-ha@e2r4p7.1337.ma>
2023-03-19 17:33:48 +0000

    add

--- ALIEN-MEGA/execute/execute.c
+++ ALIEN-MEGA/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 15:44:28 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/03/19 00:06:31 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/03/19 17:30:31 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	close_fd(int fd_in, int fd_out, int flag)
 {
 	if (flag == C_X)
 	{
-		ft_error(close(fd_in), 1);
-		ft_error(close(fd_out), 1);
+		if (isatty(fd_in) == 0)
+			ft_error(close(fd_in), 1);
+		if (isatty(fd_out) == 0)
+			ft_error(close(fd_out), 1);
 	}	
 	else if (flag == C_IN)
 		ft_error(close(fd_in), 1);
@@ -52,8 +54,7 @@ void	run_redirect(t_list *redirect)
 		ft_error(dup2(fd_in, STDIN_FILENO), 1);
 	if (fd_out != -2)
 		ft_error(dup2(fd_out, STDOUT_FILENO), 1);
-	ft_error(close(fd_in), 1);
-	ft_error(close(fd_out), 1);
+	close_fd(fd_in, fd_out, C_X);
 }
 
 void	run_command(t_list *lst)
@@ -117,8 +118,8 @@ pid_t	execute(t_tree *root, int fd_in, int fd_out, t_flag flags)
 	else if (root->lst->type == TK_PIPE)
 	{
 		fd_pipe = create_pipe();
-		execute(root->left, fd_in, fd_pipe.fd_wr, (t_flag) { .should_wait = FALSE, .flag = C_OUT});
-		pross = execute(root->right, fd_pipe.fd_rd, fd_out, (t_flag) { .should_wait = FALSE, .flag = C_IN});
+		execute(root->left, fd_in, fd_pipe.fd_wr, (t_flag) { .should_wait = FALSE, .flag = C_X});
+		pross = execute(root->right, fd_pipe.fd_rd, fd_out, (t_flag) { .should_wait = FALSE, .flag = C_X});
 		if (flags.should_wait == TRUE)
 			g_pub.exit_status = wait_pross(pross);
 	}
