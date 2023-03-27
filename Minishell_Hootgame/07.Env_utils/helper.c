90736ee6c41762adafa4a67124732e3b716e51eb
El Mehdi Bennamrouche <ebennamr@e2r8p11.1337.ma>
2023-03-27 23:13:42 +0000

    modifead signal

--- Minishell_Hootgame/07.Env_utils/helper.c
+++ Minishell_Hootgame/07.Env_utils/helper.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   helper.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: ebennamr <ebennamr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:46:50 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/03/27 02:33:20 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/03/27 23:13:11 by ebennamr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,20 @@ t_loc	ini_loc(int i, int j)
 	loc.i = i;
 	loc.j = j;
 	return (loc);
+}
+void reset_io()
+{
+	char *path;
+	path = ttyname(1);
+	ft_error_str(path, 1);
+	int fd = open(path, O_RDONLY);
+	ft_error(fd, 1);
+	ft_error(dup(fd, 1), 1);
+	close(fd);
+	path = ttyname(0);
+	ft_error_str(path, 0);
+	int fd = open(path, O_RDONLY);
+	ft_error(fd, 0);
+	ft_error(dup(fd, 0),1);
+	close(fd);
 }
