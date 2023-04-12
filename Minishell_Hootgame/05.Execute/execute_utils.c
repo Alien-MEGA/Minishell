ef82910372de1aeac267978f681f7552350fe56b
Redouane Iben Hamou <reben-ha@e2r6p13.1337.ma>
2023-04-12 04:52:09 +0000

    Add

--- Minishell_Hootgame/05.Execute/execute_utils.c
+++ Minishell_Hootgame/05.Execute/execute_utils.c
@@ -6,7 +6,7 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 22:23:26 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/04/09 01:05:13 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/04/12 04:51:21 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,29 @@
 static int	handle_sig(int status)
 {
 	if (WTERMSIG(status) == SIGSTOP)
-		ft_printf(2, "Minishell: SIG: Sendable stop signal not from tty");
+		ft_printf(2, "Minishell: SIG: Sendable stop signal not from tty\n");
 	if (WTERMSIG(status) == SIGINT)
-		ft_printf(2, "Minishell: SIG: Interrupt");
+		ft_printf(2, "Minishell: SIG: Interrupt\n");
 	if (WTERMSIG(status) == SIGQUIT)
-		ft_printf(2, "Minishell: SIG: Quit");
+		ft_printf(2, "Minishell: SIG: Quit\n");
 	if (WTERMSIG(status) == SIGABRT)
-		ft_printf(2, "Minishell: SIG: Abort");
+		ft_printf(2, "Minishell: SIG: Abort\n");
 	if (WTERMSIG(status) == SIGFPE)
-		ft_printf(2, "Minishell: SIG: Floating point exception ");
+		ft_printf(2, "Minishell: SIG: Floating point exception \n");
 	if (WTERMSIG(status) == SIGKILL)
-		ft_printf(2, "Minishell: SIG: Kill (cannot be caught or ignored)");
+		ft_printf(2, "Minishell: SIG: Kill (cannot be caught or ignored)\n");
 	if (WTERMSIG(status) == SIGBUS)
-		ft_printf(2, "Minishell: SIG: Bus error");
+		ft_printf(2, "Minishell: SIG: Bus error\n");
 	if (WTERMSIG(status) == SIGSEGV)
-		ft_printf(2, "Minishell: SIG: Segmentation violation ");
+		ft_printf(2, "Minishell: SIG: Segmentation violation \n");
 	if (WTERMSIG(status) == SIGSYS)
-		ft_printf(2, "Minishell: SIG: Bad argument to system call");
+		ft_printf(2, "Minishell: SIG: Bad argument to system call\n");
 	if (WTERMSIG(status) == SIGPIPE)
-		ft_printf(2, "Minishell: SIG: Write on a pipe with no one to read it");
+		ft_printf(2, "Minishell: SIG: Write on a pipe with no one to read it\n");
 	if (WTERMSIG(status) == SIGTERM)
-		ft_printf(2, "Minishell: SIG: Software termination signal from kill");
+		ft_printf(2, "Minishell: SIG: Software termination signal from kill\n");
 	if (WTERMSIG(status) == SIGCHLD)
-		ft_printf(2, "Minishell: SIG: To parent on child stop or exit");
+		ft_printf(2, "Minishell: SIG: To parent on child stop or exit\n");
 	return (128 + WTERMSIG(status));
 }
 
